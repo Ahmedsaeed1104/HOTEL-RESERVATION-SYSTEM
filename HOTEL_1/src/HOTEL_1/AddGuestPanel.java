ea3a69629106eb2cdc3555d95e31788a0eeace16
Ahmedsaeed1104 <168998929+Ahmedsaeed1104@users.noreply.github.com>
2025-02-24 20:43:31 +0200

    AddGuestPanel.java

--- HOTEL_1/src/HOTEL_1/AddGuestPanel.java
+++ HOTEL_1/src/HOTEL_1/AddGuestPanel.java
@@ -2,37 +2,14 @@ package HOTEL_1;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
-
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.*;
 
-
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-
-
-
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 
 public class AddGuestPanel extends JPanel {
     private JTextField nameField, phoneField, idField, roomField;
