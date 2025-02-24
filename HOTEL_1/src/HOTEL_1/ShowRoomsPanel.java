ba41efd3e4d669dcb2d39cf770c42d1dbe82e4d0
Ahmedsaeed1104 <168998929+Ahmedsaeed1104@users.noreply.github.com>
2025-02-24 20:48:29 +0200

    ShowRoomsPanel.java

--- HOTEL_1/src/HOTEL_1/ShowRoomsPanel.java
+++ HOTEL_1/src/HOTEL_1/ShowRoomsPanel.java
@@ -6,19 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.util.ArrayList;
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.IOException;
-import java.util.ArrayList;
 
-
-
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionListener;
-import java.util.ArrayList;
 
 public class ShowRoomsPanel extends JPanel {
     private final JTable roomsTable;
